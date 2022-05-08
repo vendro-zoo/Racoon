f91f151b4f976fca24614aacfc573f354bd00f9a
alessandro <alessandrovezzoli1@gmail.com>
2022-05-08 15:40:20 +0200

    ParameterCaster defines the return type also

--- src/main/kotlin/commons/casting/ParameterCaster.kt
+++ src/main/kotlin/commons/casting/ParameterCaster.kt
@@ -1,15 +1,16 @@
 package commons.casting
 
 /**
- * A class that casts a query parameter of a given type to a String.
- * The returned String is used as the value of the query parameter.
+ * A class that casts a query parameter of a given type to another given type.
+ * @param [T] the type of the parameter to cast
+ * @param [K] the type to cast to
  */
-interface ParameterCaster<T> {
+interface ParameterCaster<T: Any, K: Any> {
     /**
-     * Returns a String representation of the given parameter.
-     * @param parameter the parameter to convert to a String
-     * @return a String representation of the given parameter
+     * Converts the given parameter to another type.
+     * @param parameter the parameter to convert
+     * @return the same parameter converted to the other type
      * that can be used as the value of a query parameter.
      */
-    fun cast(parameter: T): String
+    fun cast(parameter: T): K
 }
