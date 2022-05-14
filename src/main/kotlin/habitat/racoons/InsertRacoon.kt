bbaeaf919dbc053bb6a2af3eed11a425925b1b77
alessandro <alessandrovezzoli1@gmail.com>
2022-05-14 17:59:07 +0200

    added InsertRacoon.kt as empty template class

---
+++ src/main/kotlin/habitat/racoons/InsertRacoon.kt
@@ -0,0 +1,9 @@
+package habitat.racoons
+
+import habitat.RacoonManager
+
+class InsertRacoon(manager: RacoonManager, query: String) : Racoon<InsertRacoon>(manager, query) {
+    override fun execute(): InsertRacoon {
+        TODO("Not yet implemented")
+    }
+}
