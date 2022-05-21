b91bde7fa179075886cb46d5d2d22310f8a05c6f
alessandro <alessandrovezzoli1@gmail.com>
2022-05-21 21:19:04 +0200

    added sonarqube to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.20"
+    id("org.sonarqube") version "3.3"
 }
 
 group = "org.example"
@@ -23,4 +24,12 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "vendro-zoo_racoon")
+        property("sonar.organization", "vendro-zoo")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
