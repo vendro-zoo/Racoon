b03757fdc7a96797a48043c01ec05715ea98c3ef
a-vezzoli-02 <a.vezzoli@nolecom-id.it>
2022-05-31 17:37:34 +0200

    fixed ColumnName.kt annotation target

--- src/main/kotlin/habitat/definition/ColumnName.kt
+++ src/main/kotlin/habitat/definition/ColumnName.kt
@@ -4,7 +4,7 @@ import habitat.configuration.RacoonConfiguration
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.findAnnotation
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
 annotation class ColumnName(val name: String) {
