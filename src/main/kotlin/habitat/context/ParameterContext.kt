950f6691c46c984d431cef76fa1582fae4725cbf
alessandro <alessandrovezzoli1@gmail.com>
2022-06-05 13:36:15 +0200

    added ParameterContext.kt and RacoonContext.kt docs and todos

--- src/main/kotlin/habitat/context/ParameterContext.kt
+++ src/main/kotlin/habitat/context/ParameterContext.kt
@@ -3,7 +3,17 @@ package habitat.context
 import habitat.RacoonManager
 import kotlin.reflect.KClass
 
+
+// TODO: Update to use KType instead of KClass
+/**
+ * An extension of [RacoonContext] that also provides the actual class to cast to.
+ *
+ * @see RacoonContext
+ */
 class ParameterCasterContext (
     manager: RacoonManager,
+    /**
+     * The actual class to cast to.
+     */
     val actualType: KClass<*>
 ) : RacoonContext(manager)
--- src/main/kotlin/habitat/context/RacoonContext.kt
+++ src/main/kotlin/habitat/context/RacoonContext.kt
@@ -2,6 +2,12 @@ package habitat.context
 
 import habitat.RacoonManager
 
+/**
+ * A context that is used to access in-use variables during the execution of various middleware.
+ */
 open class RacoonContext (
+    /**
+     * The in-use [RacoonManager] instance.
+     */
     val manager: RacoonManager
 )
