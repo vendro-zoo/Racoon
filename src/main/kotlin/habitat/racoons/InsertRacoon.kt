9da173db19d166f0b458e818244917af596ec76e
alessandro <alessandrovezzoli1@gmail.com>
2022-06-05 14:07:56 +0200

    updated InsertRacoon.kt docs

--- src/main/kotlin/habitat/racoons/InsertRacoon.kt
+++ src/main/kotlin/habitat/racoons/InsertRacoon.kt
@@ -6,8 +6,16 @@ import habitat.RacoonManager
  * Behaves like an [ExecuteRacoon], but also stores the last inserted id.
  */
 class InsertRacoon(manager: RacoonManager, query: String) : ExecuteRacoon(manager, query) {
+    /**
+     * The id of the last inserted row.
+     *
+     * If the query has not been executed yet, this will be `null`.
+     */
     var lastId: Int? = null
 
+    /**
+     * Executes the query to the database and saves the last inserted id.
+     */
     override fun execute() = apply {
         // Executes query
         super.execute()
