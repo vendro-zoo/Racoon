82ae039a6b1cdb692171e7da36dbbd8a5e1024f3
alessandro <alessandrovezzoli1@gmail.com>
2022-06-05 13:30:26 +0200

    added docs to RacoonConfiguration.kt

--- src/main/kotlin/habitat/configuration/RacoonConfiguration.kt
+++ src/main/kotlin/habitat/configuration/RacoonConfiguration.kt
@@ -28,9 +28,33 @@ object RacoonConfiguration {
         )
     }
 
+    /**
+     * All the configuration for the default mapping of tables and columns names.
+     */
     object Naming {
+        /**
+         * A lambda that will be used to map the alias of the table from the name of a given class.
+         *
+         * The default lambda is a function that returns the name of the class as-is.
+         *
+         * @see [internals.mappers.TableAliasMapper] for a list of built-in mappers.
+         */
         var tableAliasMapper: (String) -> String = { it }
+        /**
+         * A lambda that will be used to map the name of the table from the name of a given class.
+         *
+         * The default lambda is a function that returns the name of the class as-is.
+         *
+         * @see [internals.mappers.NameMapper] for a list of built-in mappers.
+         */
         var tableNameMapper: (String) -> String = { it }
+        /**
+         * A lambda that will be used to map the name of the column from the name of a given property.
+         *
+         * The default lambda is a function that returns the name of the property as-is.
+         *
+         * @see [internals.mappers.NameMapper] for a list of built-in mappers.
+         */
         var columnNameMapper: (String) -> String = { it }
     }
 
@@ -40,10 +64,36 @@ object RacoonConfiguration {
             Enum::class to EnumCaster()
         )
 
+        /**
+         * Adds the given [ParameterCaster] to the list of registered list of [ParameterCaster]s.
+         *
+         * If a caster has already been registered for the given [KClass], it will be replaced.
+         *
+         * Example:
+         * ```
+         * // Adds a custom caster for [LazyId]
+         * RacoonConfiguration.Casting.setCaster(LazyId::class, LazyCaster())
+         * ```
+         *
+         * @param clazz The class of the parameter to cast.
+         * @param caster The caster to use to cast the parameter.
+         * @see [internals.casting.ParameterCaster]
+         */
         fun <T: Any> setCaster(clazz: KClass<T>, caster: ParameterCaster<T, out Any>) = apply {
             parameterCasters[clazz] = caster
         }
 
+        /**
+         * Returns the [ParameterCaster] for the given [KClass].
+         *
+         * If no caster has been registered for the given [KClass],
+         * it will try searching for a caster for the superclasses of the given [KClass].
+         * If still no caster is found, it will return `null`.
+         *
+         * @param clazz The class of the parameter to cast.
+         * @return The [ParameterCaster] for the given [KClass] or `null` if no caster is found.
+         * @see [internals.casting.ParameterCaster]
+         */
         fun getCaster(clazz: KClass<*>): ParameterCaster<Any, Any>? {
             @Suppress("UNCHECKED_CAST")
             return (parameterCasters[clazz] as ParameterCaster<Any, Any>?)
