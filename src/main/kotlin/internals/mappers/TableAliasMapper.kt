e8de0c31388f0d1c9afe9e854821f6b666df068f
a-vezzoli-02 <a.vezzoli@nolecom-id.it>
2022-07-28 17:18:07 +0200

    added TableAliasMapper functions

--- src/main/kotlin/internals/mappers/TableAliasMapper.kt
+++ src/main/kotlin/internals/mappers/TableAliasMapper.kt
@@ -6,6 +6,29 @@ package internals.mappers
 @Suppress("unused")
 object TableAliasMapper {
     /**
+     * Extracts only the non-sequential uppercase letters from the given string, and converts the resulting string to lowercase.
+     *
+     * @return the extracted non-sequential uppercase letters from the given string, converted to lowercase.
+     */
+    val onlyUpperNonSequentialToLower: (String) -> String = { onlyUpperNonSequential(it).lowercase() }
+
+    /**
+     * Extracts only the non-sequential uppercase letters from the given string.
+     *
+     * @return The extracted non-sequential uppercase letters.
+     */
+    val onlyUpperNonSequential: (String) -> String = { s ->
+        s.withIndex().filter { (index, char) ->
+            char.isUpperCase() && (
+                    index == 0 ||
+                            index > 0 && s[index - 1].isLowerCase() ||
+                            index < s.length - 1 && s[index + 1].isLowerCase()
+
+                    )
+        }.map { it.value }.joinToString("")
+    }
+
+    /**
      * Extracts only the uppercase letters from the given string, and converts the resulting string to lowercase.
      *
      * @return the extracted uppercase letters from the given string, converted to lowercase.
